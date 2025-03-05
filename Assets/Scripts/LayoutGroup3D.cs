1087efcf32ba4785f65b52dcb048258463560bf7
Julia Costa <juliacosta232005@gmail.com>
2025-03-05 10:29:02 +0000

    Add Ability to Center Child Objects in LayoutGroup3D

--- Assets/Scripts/LayoutGroup3D.cs
+++ Assets/Scripts/LayoutGroup3D.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class LayoutGroup3D : MonoBehaviour
 {
     [SerializeField] private bool _horizontal;
+    [SerializeField] private bool _centerObjects;
     [SerializeField] private float _spacing;
     private List<GameObject> _children;
 
@@ -27,29 +28,50 @@ public class LayoutGroup3D : MonoBehaviour
     {
         if (transform.childCount > 0)
         {
+            // Get the position Vector of the parent
             Vector3 offsetVector = transform.position;
 
             foreach (GameObject child in _children)
             {
                 if (child.transform.GetSiblingIndex() > 0)
                 {
-                    child.transform.position = offsetVector;
-
+                    // Define offsetValue variable as zero
                     Vector3 offsetValue = Vector3.zero;
                     
+                    // Get the current child objects size
                     Vector3 childSize = GetChildSize(child);
                     
+                    // Check if we wznt horizontal or vertical alignment and offset the right value
                     if (_horizontal)
                         offsetValue.x = childSize.x + _spacing;
                     else
                         offsetValue.y = childSize.y + _spacing;
 
+                    // Add the offsetValue to the offsetVector
                     offsetVector += offsetValue;
                 }
 
+                // Apply the offsetVector to the childs position
                 child.transform.position = offsetVector;
 
                 Debug.Log($"Child {child.transform.GetSiblingIndex()} offseted by {offsetVector}");
+            }
+
+            // Align the children to the middle
+
+            if (_centerObjects)
+            {
+                foreach (GameObject child in _children)
+                {
+                    if (transform.childCount > 1)
+                    {
+                        // Divide the final offsetVector by 2 (get half the length)
+                        Vector3 alignedVector = offsetVector / 2;
+                        
+                        // Move the child object by the alignedVector value
+                        child.transform.position -= alignedVector;
+                    }
+                }
             }
         }
     }
